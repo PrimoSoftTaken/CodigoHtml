e0500408c7795bbea18f4b4a74c663047555e951
khronos <jeanvesu@gmail.com>
2023-03-22 19:26:58 -0500

    actualización con los ultimos cambios

--- controller/crud.js
+++ controller/crud.js
@@ -1,29 +1,53 @@
 const conexion = require('../database/conexion');
 
-exports.save = (req,res)=>{
-    /* capturas las celdas requeridas por el id */
-    const asambleaId = (req.body.asambleaId);
-    const delegadoId = (req.body.delegadoId);
-    const insert = `INSERT INTO emodel.asistencia_asamblea (asamblea_id, delegado_id) VALUES ('${asambleaId}','${delegadoId}')`;
-    /* prueba de captura los datos */
-    /* console.log(asambleaId + " - " + delegadoId); */
+exports.save = (req, res) => {
+    //* Captura las celdas requeridas por el id */
+    const asambleaId = req.body.asambleaId;
+    const delegadoId = req.body.delegadoId;
 
-    /* Query de inserción a la base de datos de los campos "asamblea_id" y "delegado_id"*/
-    conexion.query (insert, (error, results) => {
-        if (error){
-            throw error;
-        }else{
-            res.redirect('/');
+    /* query que busca la fecha de registro validado contra el id del usuario */
+    const fechareg = `SELECT a.asamblea_id, d.delegado_id ,d.delegado_documento_identificacion , d.delegado_nombres , d.delegado_tipo, aa.fecha_hora_registro_entrada 
+    FROM emodel.asamblea a, emodel.delegado d, emodel.asistencia_asamblea aa 
+    where aa.delegado_id = d.delegado_id and aa.asamblea_id = a.asamblea_id and  
+    aa.delegado_id = '${delegadoId}'`;
+  
+    /* Query de que inserta los en la tabla asistencia_asamblea los datos recibidos*/
+    const insert = `INSERT INTO emodel.asistencia_asamblea (asamblea_id, delegado_id) VALUES ('${asambleaId}','${delegadoId}')`;
+  
+    /* método que valida si el campo fecha esta vacío y procede con el registro o devuelve indicando con un mensaje lo sucedido */
+    conexion.query(fechareg, (error, results) => {
+      if (error) {
+        console.log(error);
+        res.redirect('/');
+      } else {
+        if (results.fecha_hora_registro_entrada != "") {
+          conexion.query(insert, (error, results) => {
+            if (error) {
+              console.log(error);
+              const message = 'El usuario ya ha registrado su asistencia';
+              res.send(`<script>if(confirm('${message}')){window.location.href='/'}</script>`);
+            } else {
+              console.log('registro satisfactorio')
+              const message = 'La asistencia ha sido registrada satisfactoriamente';
+              res.send(`<script>if(confirm('${message}')){window.location.href='/'}</script>`);
+            }
+          });
+        } else {
+          console.log('Usuario ya existe');
+          const message = 'El usuario ya ha registrado su asistencia';
+          res.send(`<script>if(confirm('${message}')){window.location.href='/'}</script>`);
         }
+      }
     });
-}
+  }
 
 exports.read = (req,res)=>{
-    /* prueba de captura los datos */
+    /* se asigna la cédula recibida a una constante */
     const cedula = (req.body.cedula);
-    const search = `SELECT a.asamblea_id, d.delegado_id ,d.delegado_documento_identificacion , d.delegado_nombres , d.delegado_tipo FROM emodel.asamblea a, emodel.delegado d WHERE d.delegado_documento_identificacion = '${cedula}'`;
-    
     /* Query de búsqueda a la base de datos por el número de cedula*/
+    const search = `SELECT a.asamblea_id, d.delegado_id ,d.delegado_documento_identificacion , d.delegado_nombres , d.delegado_tipo FROM emodel.asamblea a, emodel.delegado d WHERE  d.delegado_documento_identificacion = '${cedula}'`;
+    
+    /* método que ejecuta el query y devuelve el resultado o el error obtenidos */
     conexion.query(search, (error, results) =>{
         if (error){
             throw error;
@@ -31,4 +55,57 @@ exports.read = (req,res)=>{
             res.render('consulta', {results:results.rows});
         }
     });
+}
+
+exports.pregunta = (req, res) => {
+    /* Query de búsqueda una pregunta especifica por ID y envía el enunciado y el # de la pregunta*/
+    const idPregunta = (req.body.pregunta_id);
+    
+    //const TexPregunta = `SELECT pa.pregunta_id, pa.orden_pregunta, pa.pregunta_enunciado FROM emodel.pregunta_asamblea pa WHERE pa.pregunta_id = '${idPregunta}'`;
+
+    const TexPregunta = `SELECT pa.pregunta_id, pa.orden_pregunta, pa.pregunta_enunciado, po.pregunta_opcion_ordinal || po.pregunta_opcion_enunciado AS opcion_enunciado, crpm.votos_opcion FROM emodel.pregunta_asamblea pa INNER JOIN emodel.pregunta_opciones po ON pa.pregunta_id = po.pregunta_id LEFT OUTER JOIN emodel.calcula_resultado_pregunta_mayoria crpm ON crpm.opcion_id = po.pregunta_opcion_id WHERE pa.pregunta_id = '${idPregunta}' ORDER BY po.pregunta_id, po.pregunta_opcion_orden;`;
+    
+    conexion.query(TexPregunta, (error, results) => {
+        if (error) {
+            throw error;
+        } else {
+            res.render('view_Selec_question', {results: results.rows});
+        }
+    });
+}
+
+
+exports.salaInOut = (req,res) => {
+  const evento = (req.body.evento);
+  const alterno = (req.body.alterno);
+  const inOut = `call emodel.registrar_evento_asistencia_asamblea ('${evento}','${alterno}')`;
+
+  /* console.log(evento+" - "+ alterno);
+  const message = 'el usuario de código '+ alterno + "estado "+ evento ;
+  res.send(`<script>if(confirm('${message}')){window.location.href='/checkInOut'}</script>`); */
+
+  if (evento === "SALIDA"){
+    conexion.query(inOut, (error, results) => {
+      if (error) {
+        console.log(error);
+        const message = 'el usuario se encuentra fuera de la sala';
+        res.send(`<script>if(confirm('${message}')){window.location.href='/checkInOut'}</script>`);
+      } else {
+        console.log(error);
+        const message = 'el usuario se retira de la sala';
+        res.send(`<script>if(confirm('${message}')){window.location.href='/checkInOut'}</script>`);
+      }
+    });
+  }else{
+    conexion.query(inOut, (error, results) => {
+      if (error) {
+        console.log(error);
+        const message = 'el usuario se encuentra en la sala';
+        res.send(`<script>if(confirm('${message}')){window.location.href='/checkInOut'}</script>`);
+      } else {
+        const message = 'el usuario reingresa a la sala';
+        res.send(`<script>if(confirm('${message}')){window.location.href='/checkInOut'}</script>`);
+      }
+    });
+  }
 }
--- router.js
+++ router.js
@@ -13,9 +13,10 @@ router.get('/',(req,res)=> {
     }); */
 })
 
-/* redirecciona a la pagina donde aparece el listado completo de delegados */
-router.get('/general',(req,res)=>{
-    conexion.query('select d.delegado_id ,d.delegado_documento_identificacion , d.delegado_nombres , d.delegado_tipo ,aa.fecha_hora_registro_entrada from emodel.delegado d left outer join emodel.asistencia_asamblea aa on d.delegado_id  = aa.delegado_id order by aa.fecha_hora_registro_entrada  asc', (error,results)=>{
+/* redirecciona a la pagina donde aparece el listado completo de delegados y lo ordena por fecha y hora de registro de ingreso a la Asamblea*/
+router.get('/general', (req, res) => {
+    const lgeneral = 'select d.delegado_id ,d.delegado_documento_identificacion , d.delegado_nombres , d.delegado_tipo ,aa.fecha_hora_registro_entrada from emodel.delegado d left outer join emodel.asistencia_asamblea aa on d.delegado_id  = aa.delegado_id order by aa.fecha_hora_registro_entrada  asc';
+    conexion.query(lgeneral , (error,results)=>{
         if (error){
             throw error;
         }else{
@@ -32,12 +33,42 @@ router.get('/consulta', (req,res)=>{
     res.render('consulta');
 })
 
+router.get('/checkInOut', (req,res)=>{
+    res.render('checkInOut');
+})
+
 const crud = require ('./controller/crud');
 router.post('/save', crud.save);
 router.post('/read', crud.read);
+router.post('/pregunta', crud.pregunta);
+router.post('/salaInOut', crud.salaInOut);
 
-router.get('/contacto',(req,res)=>{
-    res.send('Este es para una de las vistas');
+//enrutamiento para visualizar todas las preguntas 
+router.get('/view_questions', (req, res) => {
+    const lViewAll = `select pregunta_id, orden_pregunta, pregunta_enunciado,case bandera_votacion when 'E' then 'En espera de votación' when 'C' then 'Pregunta Votada' when 'A' then 'Pregunta en proceso de votación' end estado_pregunta from emodel.pregunta_asamblea pa order by pregunta_id`;
+
+    conexion.query(lViewAll , (error,results)=>{
+        if (error){
+            throw error;
+        } else {
+            res.render('view_questions', { results: results.rows });
+        }
+    });
 });
+
+
+//enrutamiento para visualizar los delegados presentes en la asamblea para validar el Quorum
+router.get('/estadoEnSala', (req, res) => {
+    const courum = `select d.delegado_documento_identificacion ,d.delegado_nombres ,aa.fecha_hora_registro_entrada , case aa.asistente_activo when true then 'EN SALA' when false then 'FUERA DE SALA' end estado_asistencia from emodel.delegado d inner join emodel.asistencia_asamblea aa on d.delegado_id = aa.delegado_id and aa.asamblea_id = 1 order by estado_asistencia, fecha_hora_registro_entrada`;
+
+    conexion.query(courum , (error,results)=>{
+        if (error){
+            throw error;
+        } else {
+            res.render('estadoEnSala', { results: results.rows });
+        }
+    });
+ 
+})
 
 module.exports = router;
